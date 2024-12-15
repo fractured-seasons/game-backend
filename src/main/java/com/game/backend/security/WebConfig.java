e120dc4ebb7a69a79d54badfac84d1657d1917d4
0xDRAGOS <sdragos231@gmail.com>
2024-12-15 16:15:01 +0200

    add contact endpoint; allow OPTIONS requests;

--- src/main/java/com/game/backend/security/WebConfig.java
+++ src/main/java/com/game/backend/security/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig implements WebMvcConfigurer {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
+                registry.addMapping("/api/**")
                         .allowedOrigins(frontendUrl)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
